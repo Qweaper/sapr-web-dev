6fe1767b70cd10f1d5c5eaa5fec8dc3c5b186db6
Vasiliev Vlad <chief.vlavas@yandex.ru>
2023-04-28 17:41:47 +0300

    Some refractoring code in lab_7

--- labs/lab_7/app.js
+++ labs/lab_7/app.js
@@ -3,10 +3,6 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 
 const {apiVerRouter} = require('./router.js');
-const {
-    genTable,
-    data
-} = require('../lab_5/tools');
 
 const app = express();
 const host = '127.0.0.1'
@@ -15,31 +11,10 @@ const port = '3000'
 //Добавляем Morgan
 app.use(morgan('dev'))
 
-
 //Добавляем Helmet
 app.use(helmet());
-app.disable('x-powered-by');
 
 app.use('/API/1/', apiVerRouter);
-
-apiVerRouter.get('/login/:password', (req, res, next) => {
-    let password = req.params['password'];
-    if (password === 'secret') 
-    {
-        res.statusCode = 200;
-        res.write('Access allowed!');
-        // res.redirect('/API/1/about');
-        // next();
-
-    }
-    else 
-    {
-        res.statusCode=403;
-        res.write('Forbidden!')
-    }
-    res.send()
-
-});
 
 app.use((req, res, next) => {
     console.log("Method: ", req.method);
@@ -50,44 +25,7 @@ app.use((req, res, next) => {
     next();
 });
 
-apiVerRouter.use((req, res, next) => {
-    let headers = req.headers;
-    if (headers.hasOwnProperty('user-agent')) {
-        let user_agent_data = headers['user-agent'];
-        if (!data['user-agent-stats'].hasOwnProperty(user_agent_data)){
-            data['user-agent-stats'][user_agent_data] = 0;
-        }
-        data['user-agent-stats'][user_agent_data] += 1;
-    }
-    next();
-});
-
-apiVerRouter.use(express.static('./public'));
-
-apiVerRouter.get('/about', () => {
-    req.redirect('/API/1/index.html')
-})
-
-// можно делать через handlebars - шаблонизатор
-apiVerRouter.get('/stats',(req, res) => {
-        res.send(genTable(data['user-agent-stats']));
-});
-
-apiVerRouter.post('/comments', express.json(), (req, res) => {
-    let body = req.body;
-    data['comments'].push(...body['comments']);
-    res.send(JSON.stringify(data['comments']));
-});
-
-apiVerRouter.get('/', (req, res) => {
-    res.send('This is my server on Express.JS\nHello Everyone!');
-});
-
 app.use((req, res) => {
-    res.status(400).send('Bad request!');
-});
-
-apiVerRouter.use((req, res) => {
     res.status(400).send('Bad request!');
 });
 
--- labs/lab_7/router.js
+++ labs/lab_7/router.js
@@ -5,6 +5,55 @@ const {
 } = require('../lab_5/tools');
 const apiVerRouter = express.Router();
 
+apiVerRouter.use(express.static('./public'));
+
+apiVerRouter.use((req, res, next) => {
+    let headers = req.headers;
+    if (headers.hasOwnProperty('user-agent')) {
+        let user_agent_data = headers['user-agent'];
+        if (!data['user-agent-stats'].hasOwnProperty(user_agent_data)){
+            data['user-agent-stats'][user_agent_data] = 0;
+        }
+        data['user-agent-stats'][user_agent_data] += 1;
+    }
+    next();
+});
+
+// параметры            -- /home/user/:id <=> /home/user/8     => req.params; >>> {...}
+// строка аргументов    -- /home/user     <=> /home/user?id=8  => req.query;  >>> {...}
+apiVerRouter.get('/login', (req, res, next) => { // переписать на аргументы строки
+    let password = req.query['password'];
+    if (password === 'secret') 
+    {
+        res.redirect('/API/1/about');
+    } else {
+        res.statusCode=403;
+        res.send('Forbidden!');
+    }    
+});
+
+apiVerRouter.get('/about', (req, res) => {
+    res.redirect('/API/1/index.html')
+})
+
+// можно делать через handlebars - шаблонизатор
+apiVerRouter.get('/stats',(req, res) => {
+        res.send(genTable(data['user-agent-stats']));
+});
+
+apiVerRouter.post('/comments', express.json(), (req, res) => {
+    let body = req.body;
+    data['comments'].push(...body['comments']);
+    res.send(JSON.stringify(data['comments']));
+});
+// проверка на наличие неправильных данных в json
+
+
+apiVerRouter.get('/', (req, res) => {
+    res.send('This is my server on Express.JS\nHello Everyone!');
+});
+
+
 module.exports = {
     apiVerRouter
 };
